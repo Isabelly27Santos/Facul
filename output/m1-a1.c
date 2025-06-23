66b38005bbda9e8847bc63a7e3dda83d42403641
Isabelly Santos <isabelly27js@gmail.com>
2025-06-23 12:50:10 -0300

    m1-a1

---
+++ output/m1-a1.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    int *ponteiro;
+    setlocale(LC_ALL,"Portuguese_Brazil");
+    ponteiro=(int*)malloc(sizeof(int));
+    if(ponteiro==NULL){
+        printf("Erro!");
+    }
+    else{
+        *ponteiro=42;
+        printf("Endereço do ponteiro: %i\nValor: %i\n",&ponteiro,*ponteiro);
+    }    
+    return 0;
+}
+
+
+
